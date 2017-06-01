32dcb629a8d207823cabd327ab281a6029795c36
Zhangliang Dong <zhanglid@usc.edu>
2017-06-01 15:01:05 -0700

    Update test script

--- week2/HW1_updated/autotest.py
+++ week2/HW1_updated/autotest.py
@@ -1,14 +1,15 @@
 import os
 from os import listdir
-
+import re
+input_format = re.compile(r't\d+\.txt')
 file_list = listdir('./testcases')
 input_list = []
+
 for f_name in file_list:
-    if f_name == 'output.txt':
-        os.system('rm testcases/output.txt')
-    if f_name.find('txt') and f_name.find('_') < 0:
+    if input_format.match(f_name):
         input_list.append(f_name)
 
+total_pass = len(input_list)
 for input_file_name in input_list:
     print input_file_name
     os.system('python hw1cs561s17.py -i ' + 'testcases/' + input_file_name)
@@ -19,6 +20,9 @@ for input_file_name in input_list:
     print output
     print right_output
     if output != right_output:
+        total_pass -= 1
         print '!!!!!!!!!!!!!!!!!!!!!'
     print output == right_output
     print '--------------------------'
+
+print 'pass/total: ' + str(total_pass) + '/' + str(len(input_list))
